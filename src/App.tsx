6a09fdc91b62d2ada710a9db30c5df26292a0b4a
klajdi44 <klajdi1996@yahoo.gr>
2021-04-23 22:29:55 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+import './App.scss';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className='App'>
+      <h1>hello peeps</h1>
+    </div>
+  );
+}
+
+export default App;
