24537f698b5774211b743f438c9fc3f58d9a09d5
klajdi44 <klajdi1996@yahoo.gr>
2021-05-11 14:08:47 +0200

    added functionality for mobile nav

---
+++ src/components/Assets/Home.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Props } from './Feed';
+function Home(props: Props) {
+  return (
+    <svg
+      id='Layer_1'
+      data-name='Layer 1'
+      xmlns='http://www.w3.org/2000/svg'
+      viewBox='0 0 71.66 51.97'
+      className={`nav_svg ${props.className}`}
+    >
+      <path
+        d='M46.89,41.2a3.39,3.39,0,0,1,3.39-3.39h.55a3.39,3.39,0,0,1,3.39,3.39v8.4H66.34L49.92,11.7,33.51,49.6H46.89Z'
+        fill='#1a1a1a'
+        className={props.className}
+      />
+      <path
+        d='M50.89,2.15H23.11Q11.56,26,0,49.93H71.33Q61.11,26,50.89,2.15ZM24,9H43v1.11H24Zm-4.23,8.81H38.81V18.9h-19ZM16.17,26.6H35.23v1.11H16.17Zm-4.33,8.82H30.9v1.11H11.84ZM27.1,45.28h-19V44.17H27.1ZM49.92,5,69.22,49.6H30.63Z'
+        fill='#1a1a1a'
+        className={props.className}
+      />
+    </svg>
+  );
+}
+
+export default Home;
--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,21 +1,91 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import Home from '../Assets/Home';
+import Feed from '../Assets/Feed';
+import Investment from '../Assets/Investment';
+import Messages from '../Assets/Messages';
 
 function Nav() {
+  const [selected, setSelected] = useState<string>('home');
+  const [showNavLi, setShowNavLi] = useState(false);
+  useEffect(() => {
+    window.addEventListener('resize', () => {
+      if (window.matchMedia('(min-width: 700px)').matches) {
+        setShowNavLi(true);
+      } else {
+        setShowNavLi(false);
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    if (window.matchMedia('(min-width: 700px)').matches) {
+      setShowNavLi(true);
+    }
+  }, []);
+
+  function handleClick(selected: string): void | Error {
+    switch (selected) {
+      case 'home':
+        return setSelected('home');
+      case 'feed':
+        return setSelected('feed');
+      case 'messages':
+        return setSelected('messages');
+      case 'investment':
+        return setSelected('investment');
+      case 'profile':
+        return setSelected('profile');
+      default:
+        return new Error('failed to change selected nav value');
+    }
+  }
+
   return (
     <nav className='nav'>
       <ul className='nav_ul'>
-        <Link to='' className='nav_ul_a'>
-          <li className='nav_ul_a_li'>Home</li>
+        <Link
+          to=''
+          className={selected === 'home' ? 'nav_ul_a selected' : 'nav_ul_a'}
+          onClick={() => handleClick('home')}
+        >
+          <Home className={selected === 'home' ? 'selected' : ''} />
+          {showNavLi && <li className='nav_ul_a_li'>Home</li>}
         </Link>
-        <Link to='' className='nav_ul_a'>
-          <li className='nav_ul_a_li'>Feed</li>
+        <Link
+          to=''
+          className={selected === 'feed' ? 'nav_ul_a selected' : 'nav_ul_a'}
+          onClick={() => handleClick('feed')}
+        >
+          <Feed className={selected === 'feed' ? ' feed selected' : 'feed'} />
+          {showNavLi && <li className='nav_ul_a_li'>Feed</li>}
         </Link>
-        <Link to='' className='nav_ul_a'>
-          <li className='nav_ul_a_li'>Investment</li>
+        <Link
+          to=''
+          className={
+            selected === 'investment' ? 'nav_ul_a selected' : 'nav_ul_a'
+          }
+          onClick={() => handleClick('investment')}
+        >
+          <Investment className={selected === 'investment' ? 'selected' : ''} />
+          {showNavLi && <li className='nav_ul_a_li'>Investment</li>}
         </Link>
-        <Link to='' className='nav_ul_a'>
-          <li className='nav_ul_a_li'>Messages</li>
+        <Link
+          to=''
+          className={selected === 'messages' ? 'nav_ul_a selected' : 'nav_ul_a'}
+          onClick={() => handleClick('messages')}
+        >
+          <Messages className={selected === 'messages' ? 'selected' : ''} />
+          {showNavLi && <li className='nav_ul_a_li'>Messages</li>}
+        </Link>
+
+        <Link
+          to=''
+          className={selected === 'messages' ? 'nav_ul_a selected' : 'nav_ul_a'}
+          onClick={() => handleClick('profile')}
+        >
+          <Messages className={selected === 'profile' ? 'selected' : ''} />
+          {showNavLi && <li className='nav_ul_a_li'>Profile</li>}
         </Link>
       </ul>
     </nav>
