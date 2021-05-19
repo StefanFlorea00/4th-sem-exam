39e8c88b590d636f7b332cb1769da6b944b9496c
klajdi44 <klajdi1996@yahoo.gr>
2021-05-19 14:45:25 +0200

    added real time functionality

--- src/components/Messages/ChatRoom.tsx
+++ src/components/Messages/ChatRoom.tsx
@@ -1,18 +1,18 @@
 import React, { useContext, useState, useEffect } from 'react';
 import { AuthContext } from '../../Auth';
 import { getDoc } from '../FirebaseApp';
-import app from '../FirebaseApp';
 import firebase from 'firebase/app';
 
 type Props = {
+  messageCollection: firebase.firestore.CollectionReference<firebase.firestore.DocumentData>;
   selectedRoom: string;
-  setSelectedRoom: React.Dispatch<React.SetStateAction<string>>;
 };
 
 function ChatRoom(props: Props) {
   const { currentUser } = useContext(AuthContext);
   const [userInfo, setUserInfo] = useState<undefined | any>(undefined);
   const [textAreaValue, setTextAreaValue] = useState<string>('');
+  const { messageCollection, selectedRoom } = props;
 
   useEffect(() => {
     if (currentUser) {
@@ -20,22 +20,28 @@ function ChatRoom(props: Props) {
     }
   }, []);
 
+  // props.setMessagesArr(messages);
+
+  // props.setMessagesArr(useCollectionData(dbQuery, { idField: 'id' }));
+
   async function handleInput(e: React.SyntheticEvent) {
     e.preventDefault();
-    const messageCollection = app.firestore().collection('messages');
 
     if (currentUser && userInfo) {
       try {
         await messageCollection.add({
           uid: currentUser.uid,
           email: userInfo.email,
+          fullname: userInfo.fullname,
+          description: userInfo.description,
           profileImg:
             userInfo.profileImg ||
             'https://images.unsplash.com/photo-1611034540516-665df2bbdfd9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
           text: textAreaValue,
           createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-          chatroom: props.selectedRoom,
+          chatroom: selectedRoom,
         });
+        setTextAreaValue('');
       } catch (err) {
         console.log(err.message);
       }
--- src/components/Messages/Messages.tsx
+++ src/components/Messages/Messages.tsx
@@ -1,12 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ChatRoom from './ChatRoom';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+import app from '../FirebaseApp';
 
 function Messages(props) {
   const [selectedRoom, setSelectedRoom] = useState('general');
+  const messageCollection = app.firestore().collection('messages');
+
+  const dbQuery = messageCollection.where('chatroom', '==', selectedRoom);
+  const [messages] = useCollectionData(dbQuery, { idField: 'id' });
+
+  console.log(messages);
 
   return (
     <div className='messages'>
-      <ChatRoom selectedRoom={selectedRoom} setSelectedRoom={setSelectedRoom} />
+      <ChatRoom
+        selectedRoom={selectedRoom}
+        messageCollection={messageCollection}
+      />
+      {messages &&
+        messages.map(user => {
+          return (
+            <div>
+              <h1>{user.fullname}</h1>
+              <h2>{user.text}</h2>
+              <img src={user.profileImg} />
+            </div>
+          );
+        })}
     </div>
   );
 }
