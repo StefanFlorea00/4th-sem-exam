9f313e9a5802247921250cca7aaeac943748eb6f
Gintare B <gintaar3@gmail.com>
2021-05-24 17:12:38 +0200

    added posts length

--- src/components/Profile/PostList.tsx
+++ src/components/Profile/PostList.tsx
@@ -14,7 +14,7 @@ function PostList(props: any) {
 
   return (
     <div className='profile-posts'>
-      <h2 className='profile-posts_header'>Posts</h2>
+      <h2 className='profile-posts_header'>{posts?.length + ' Posts'}</h2>
       <div className='profile-posts_list'>
         {posts && posts.length !== 0 ? posts.map((post)=>{
          return <Post key={Math.random() + 'post'} uid={post.uid} content={post.content} postImage={post.media} postId={post.id}/>
