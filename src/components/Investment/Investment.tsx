235b9e68b19db241620ae72045f50d8d63f2b3ff
klajdi44 <klajdi1996@yahoo.gr>
2021-05-30 14:04:09 +0200

    fixed fetching problem

--- src/components/Investment/Investment.tsx
+++ src/components/Investment/Investment.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import app from '../FirebaseApp';
 import CompaniesGrid from './CompaniesGrid';
 import './Investment.scss';
@@ -7,11 +7,11 @@ import LoadingSVG from '../Assets/Loading';
 import Button from '../Buttons/Button';
 
 function Investment() {
-
-  const [companyList, setCompanyList] = useState([{}]);
-  const [country, setCountry] = useState("");
-  const [companyFetchNr, setCompanyFetchNr] = useState();
+  const [companyList, setCompanyList] = useState([]);
+  const [country, setCountry] = useState('United States');
+  const [companyFetchNr, setCompanyFetchNr] = useState(500);
   const [fetching, setFetching] = useState(false);
+
   function handleSignOut() {
     //sign out from the app
     app.auth().signOut();
@@ -19,67 +19,56 @@ function Investment() {
 
   useEffect(() => {
     fetchCompanies();
-  }, [])
+    setCountry('United States');
+    // setCompanyFetchNr(500);
+  }, []);
 
-  async function fetchCompanies(){
+  async function fetchCompanies() {
     const API_KEY = '44e4c120e4ab42239b2c7b36c9a4207f';
-    let  API_Call = `https://api.twelvedata.com/stocks`;
-
-    setCountry("United States");
-    setCompanyFetchNr(500);
-
+    let API_Call = `https://api.twelvedata.com/stocks`;
+    const response = await fetch(API_Call);
     setFetching(true);
-    try {
-      const response = await fetch(API_Call);
-      const json = await response.json();
-      setCompanyList(generateCompanyObjects(json));
-    } catch (e){
-      console.log(e)
-    } finally {
+    if (!response.ok) {
       setFetching(false);
+      throw new Error('something went wrong');
     }
-}
+    const data = await response.json();
+    const dataArr = await generateCompanyObjects(data);
 
-  function generateCompanyObjects(companyData){
+    setCompanyList(dataArr);
+    setFetching(false);
+  }
+
+  async function generateCompanyObjects(companyData) {
     let companies = [];
-    let fetchedCompaniesNr = 0;
-    console.log(companyData);
-    for(let i = 0 ; i< companyData.data.length ; i++ )  {
-      if(companyData.data[i].country == country && fetchedCompaniesNr < companyFetchNr){
-        companies.push(
-          {
+    for (let i = 0; i < companyData.data.length; i++) {
+      if (companyData.data[i].country == country) {
+        companies.push({
           id: companyData.data[i].symbol,
           name: companyData.data[i].name,
           field: companyData.data[i].country,
-          img:{testimg}
-          }
-          );
-        fetchedCompaniesNr++;
+          img: { testimg },
+        });
+        if (companies.length > companyFetchNr) {
+          return companies;
+        }
       }
     }
-    return companies;
   }
 
   return (
     <>
-    <div className='investment'>
-      <div className='description'>
-        <h1>Currently available Companies</h1>
-        <p>Here you can see a list of companies that are currently available to invest into. Please select a company to proceed.</p>
+      <div className='investment'>
+        <div className='description'>
+          <h1>Currently available Companies</h1>
+          <p>
+            Here you can see a list of companies that are currently available to
+            invest into. Please select a company to proceed.
+          </p>
+        </div>
+        {fetching && <LoadingSVG className='company-loading' />}
+        {companyList.length && <CompaniesGrid companies={companyList} />}
       </div>
-      {fetching ?
-        <LoadingSVG className="company-loading"/>
-        :
-        <>
-        {companyList.length == 0 ? 
-          <div className="error-div">
-            <p className="error-text">Uh oh, looks like there's been a problem</p>
-            <Button onClick={() => fetchCompanies()} type="secondary" text="Retry"/>
-          </div>
-        : <CompaniesGrid companies={companyList}/>}
-        </>
-      }
-    </div>
     </>
   );
 }
