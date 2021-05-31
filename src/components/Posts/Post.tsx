cd356ebc5bbcc0ba316581cd4ade670a600edcff
Gintare B <gintaar3@gmail.com>
2021-05-31 22:07:47 +0200

    fixed comments

--- src/components/Posts/Post.tsx
+++ src/components/Posts/Post.tsx
@@ -1,79 +1,79 @@
-import React, { useEffect, useState } from 'react';
-import UserButton from '../Buttons/UserButton';
-import Button from '../Buttons/Button';
-import './Post.scss';
-import PostWhoSaw from './PostWhoSaw';
-import { firestore } from '../FirebaseApp';
-import { getDoc } from '../FirebaseApp';
-import { Link } from 'react-router-dom';
-import * as firebase from 'firebase/app';
-import app from '../FirebaseApp';
-import ChatRoomCard from '../ChatRoom/ChatRoomCard';
-import { Comments, createdAt } from '../Home/Home';
-import Comment from '../Assets/Comment';
+import React, { useEffect, useState } from "react"
+import UserButton from "../Buttons/UserButton"
+import Button from "../Buttons/Button"
+import "./Post.scss"
+import PostWhoSaw from "./PostWhoSaw"
+import { firestore } from "../FirebaseApp"
+import { getDoc } from "../FirebaseApp"
+import { Link } from "react-router-dom"
+import * as firebase from "firebase/app"
+import app from "../FirebaseApp"
+import ChatRoomCard from "../ChatRoom/ChatRoomCard"
+import { Comments, createdAt } from "../Home/Home"
+import Comment from "../Assets/Comment"
 
 export type Props = {
-  uid: string;
-  postImage: string;
-  content: string;
-  postId?: string;
-  comments?: any;
-};
+  uid: string
+  postImage: string
+  content: string
+  postId?: string
+  comments?: any
+}
 
 type User = {
-  createdAt: createdAt;
-  description: string;
-  email: string;
-  fullname: string;
-  investExp: string;
-  profileImg: string;
-};
+  createdAt: createdAt
+  description: string
+  email: string
+  fullname: string
+  investExp: string
+  profileImg: string
+}
 function Post(props: Props) {
-  const [postUser, setPostUser] = useState<User | any>();
+  const [postUser, setPostUser] = useState<User | any>()
 
   const [postComments, setPostComments] = useState<Comments[]>(
     props.comments.slice(0, 3)
-  );
-  const [userInfo, setUserInfo] = useState<User | any>();
-  const currentUser = app.auth().currentUser;
-  const [showLoad, setShowLoad] = useState(false);
+  )
+  const [userInfo, setUserInfo] = useState<User | any>()
+  const currentUser = app.auth().currentUser
+  const [showLoad, setShowLoad] = useState(false)
 
   useEffect(() => {
     firestore
-      .collection('users')
+      .collection("users")
       .doc(props.uid)
       .get()
-      .then(data => {
-        data && setPostUser(data.data());
-      });
-    getDoc(currentUser).then(data => setUserInfo(data));
+      .then((data) => {
+        data && setPostUser(data.data())
+      })
+    getDoc(currentUser).then((data) => setUserInfo(data))
 
     if (props.comments.length > 3) {
-      setShowLoad(true);
+      setShowLoad(true)
     }
     if (props.comments.length < 5) {
-      setShowLoad(false);
+      setShowLoad(false)
     }
     if (props.comments.length === postComments.length) {
-      setShowLoad(false);
+      setShowLoad(false)
     }
-  }, []);
+  }, [])
 
   async function handleSubmit(e: any) {
-    e.preventDefault();
+    e.preventDefault()
     //@ts-ignore
-    const docId = e.target.closest('.post')?.dataset.id;
-    const { text } = e.target.elements;
+    const docId = e.target.closest(".post")?.dataset.id
+    const { text } = e.target.elements
 
     if (
       currentUser &&
       userInfo &&
       text.value.length > 0 &&
-      text.value !== ' '
+      text.value !== " "
     ) {
       try {
         await firestore
-          .collection('posts')
+          .collection("posts")
           .doc(docId)
           .update({
             comments: firebase.default.firestore.FieldValue.arrayUnion({
@@ -82,35 +82,35 @@ function Post(props: Props) {
               ...userInfo,
               createdAt: new Date(),
             }),
-          });
-        text.value = '';
+          })
+        text.value = ""
         firestore
-          .collection('posts')
+          .collection("posts")
           .doc(docId)
-          .onSnapshot(snapshot => {
-            setPostComments(snapshot.data()?.comments);
-            setShowLoad(false);
-          });
+          .onSnapshot((snapshot) => {
+            setPostComments(snapshot.data()?.comments)
+            setShowLoad(false)
+          })
       } catch (error) {
-        throw new Error(error.message);
+        throw new Error(error.message)
       }
     }
   }
 
-  function handleClick(lessOrMore: 'more' | 'less') {
-    lessOrMore === 'more'
+  function handleClick(lessOrMore: "more" | "less") {
+    lessOrMore === "more"
       ? setPostComments(props.comments.slice(0, postComments.length + 5))
-      : setPostComments(props.comments.slice(0, 3));
+      : setPostComments(props.comments.slice(0, 3))
   }
 
   return (
     <div
-      className={props.postImage ? 'post with-img' : 'post'}
+      className={props.postImage ? "post with-img" : "post"}
       data-id={props.postId}
     >
       <Link
-        to={{ pathname: '/profile/' + props.uid, state: { uid: props.uid } }}
-        style={{ textDecoration: 'none' }}
+        to={{ pathname: "/profile/" + props.uid, state: { uid: props.uid } }}
+        style={{ textDecoration: "none" }}
       >
         <UserButton
           hasInfo
@@ -119,16 +119,16 @@ function Post(props: Props) {
         />
       </Link>
       {props.postImage && (
-        <img className='post-img' src={props.postImage} alt='Post image' />
+        <img className="post-img" src={props.postImage} alt="Post image" />
       )}
-      <p className='post-content'>{props.content}</p>
+      <p className="post-content">{props.content}</p>
 
       <br />
       <hr />
-      <div className='bottom-div'>
-        <div className='bottom-div_comments_wrapper'>
+      <div className="bottom-div">
+        <div className="bottom-div_comments_wrapper">
           {postComments.length > 0 && (
-            <span className='bottom-div_comments_wrapper_title'>
+            <span className="bottom-div_comments_wrapper_title">
               All comments
               <Comment />
             </span>
@@ -138,17 +138,17 @@ function Post(props: Props) {
           </span>
         </div>
         {!postComments.length && (
-          <small className='no_comments'> No comments </small>
+          <small className="no_comments"> No comments </small>
         )}
         {props.comments &&
           postComments.map((el: Comments) => {
-            const Timestamp = firebase.default.firestore.Timestamp;
+            const Timestamp = firebase.default.firestore.Timestamp
             const getDate = new Timestamp(
               el.createdAt?.seconds,
               el.createdAt?.nanoseconds
-            ).toDate();
-            const [, month, date, year, hour] = getDate.toString().split(' ');
-            const time = `${date} ${month} ${year} ${hour}`;
+            ).toDate()
+            const [, month, date, year, hour] = getDate.toString().split(" ")
+            const time = `${date} ${month} ${year} ${hour}`
 
             return (
               <ChatRoomCard
@@ -156,30 +156,31 @@ function Post(props: Props) {
                 fullname={el.fullname}
                 profileImg={el.profileImg}
                 text={el.text}
-                uid={currentUser?.uid}
+                uid={el.commentorUid}
                 time={time}
               />
-            );
+            )
           })}
-        {showLoad && props.comments.length !== postComments.length ? (
-          <div className='load-more' onClick={() => handleClick('more')}>
+        {showLoad && props.comments.length !== postComments.length && (
+          <div className="load-more" onClick={() => handleClick("more")}>
             Load more ▼
           </div>
-        ) : (
-          <div className='load-more' onClick={() => handleClick('less')}>
-            Load less ▲
+        )}
+        {showLoad && props.comments.length === postComments.length && (
+          <div className="load-more" onClick={() => handleClick("less")}>
+            Show less ▲
           </div>
         )}
 
         <form onSubmit={handleSubmit}>
-          <div className='input-wrapper'>
-            <input type='text' name='text' placeholder='Write a comment' />
-            <Button type='primary' text='Comment' />
+          <div className="input-wrapper">
+            <input type="text" name="text" placeholder="Write a comment" />
+            <Button type="primary" text="Comment" />
           </div>
         </form>
       </div>
     </div>
-  );
+  )
 }
 
-export default Post;
+export default Post
